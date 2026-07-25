df11d91fc88eefda2dce9ccf36ec7b467c85b7e6
KzRaihan <kamruzzamanraihan00@gmail.com>
2026-07-25 21:50:55 +0600

    logger and exception handling added

---
+++ demo.py
@@ -0,0 +1,16 @@
+# loginging demo
+# -----------------------------------------------------------------
+# from shipment.logger import logging
+
+# logging.info("This is Test logging Message from demo.py")
+
+
+# exception demo
+# -----------------------------------------------------------------
+# from shipment.exception import shippingException
+# import sys # work: find the error of which line 
+
+# try:
+#     a = 1 / 0
+# except Exception as e:
+#     raise shippingException(e, sys) from e
