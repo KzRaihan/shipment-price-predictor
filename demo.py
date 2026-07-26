b22fc6bda20a8db9c51f61d959d13fc8571ddc9c
KzRaihan <kamruzzamanraihan00@gmail.com>
2026-07-26 15:23:17 +0600

    mongo_oeration : Fetch the Data From MongoBD Database is done in modular format

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
-from shipment.constants import DB_URL
+from shipment.configuration.mongo_operations import MongoDBOperation 
 
-print(DB_URL)
+obj = MongoDBOperation()
+
+df = obj.get_collection_as_dataframe(db_name="Shipment_Kz", collection_name="shipment_collection")
+
